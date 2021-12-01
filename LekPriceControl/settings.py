d62c4da509ee0ce44ab817063511f1d7a5f0cbff
alexander-kiselev-1972 <67494579+alexander-kiselev-1972@users.noreply.github.com>
2021-12-01 23:06:38 +0300

    Update settings.py

--- LekPriceControl/settings.py
+++ LekPriceControl/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ht&(_-!gadh6(&z760i!m7thy70-6y6obs!cgo(7h#o=t#&g6q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
